54ac77d49de42b0fcfd8f6d1b0962d65dc8ce913
Shift <shift@laravelshift.com>
2025-06-02 14:11:31 +0000

    Adopt class based routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,18 @@
 <?php
 
+use App\Http\Controllers\Auth;
+use App\Http\Controllers\Backend;
+use App\Http\Controllers\DemoCaseStudyController;
+use App\Http\Controllers\DirectoryController;
+use App\Http\Controllers\EventController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\LanguageController;
+use App\Http\Controllers\MediaLibraryController;
+use App\Http\Controllers\NewsController;
+use App\Http\Controllers\PageController;
+use App\Http\Controllers\ToolController;
+use Illuminate\Support\Facades\Route;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -18,139 +31,139 @@ Route::get('/', function () {
 // Auth::routes();
 
 // Authentication Routes...
-Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
-Route::post('login', 'Auth\LoginController@login');
-Route::post('logout', 'Auth\LoginController@logout')->name('logout');
-Route::get('approval', 'Auth\LoginController@showApproval')->name('login.approval');
+Route::get('login', [Auth\LoginController::class, 'showLoginForm'])->name('login');
+Route::post('login', [Auth\LoginController::class, 'login']);
+Route::post('logout', [Auth\LoginController::class, 'logout'])->name('logout');
+Route::get('approval', [Auth\LoginController::class, 'showApproval'])->name('login.approval');
 
 // Registration Routes...
-Route::get('register', 'Auth\RegisterController@showRegistrationForm')->name('register');
-Route::post('register', 'Auth\RegisterController@register');
-Route::get('thanks', 'Auth\RegisterController@showThanks')->name('register.thanks');
+Route::get('register', [Auth\RegisterController::class, 'showRegistrationForm'])->name('register');
+Route::post('register', [Auth\RegisterController::class, 'register']);
+Route::get('thanks', [Auth\RegisterController::class, 'showThanks'])->name('register.thanks');
 
 // Password Reset Routes...
-Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
-Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
-Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
-Route::post('password/reset', 'Auth\ResetPasswordController@reset')->name('password.update');
+Route::get('password/reset', [Auth\ForgotPasswordController::class, 'showLinkRequestForm'])->name('password.request');
+Route::post('password/email', [Auth\ForgotPasswordController::class, 'sendResetLinkEmail'])->name('password.email');
+Route::get('password/reset/{token}', [Auth\ResetPasswordController::class, 'showResetForm'])->name('password.reset');
+Route::post('password/reset', [Auth\ResetPasswordController::class, 'reset'])->name('password.update');
 
 // Email Verification Routes
 // Route::get('email/verify', 'Auth\VerificationController@show')->name('verification.notice');
 // Route::get('email/verify/{id}', 'Auth\VerificationController@verify')->name('verification.verify');
 // Route::get('email/resend', 'Auth\VerificationController@resend')->name('verification.resend');
 
-Route::get('/', 'HomeController@index')->name('home');
+Route::get('/', [HomeController::class, 'index'])->name('home');
 
-Route::resource('directory', 'DirectoryController')->only('show');
-Route::post('directory/list', 'DirectoryController@index')->name('directory.list');
-Route::post('directory/map', 'DirectoryController@map')->name('directory.map');
+Route::resource('directory', DirectoryController::class)->only('show');
+Route::post('directory/list', [DirectoryController::class, 'index'])->name('directory.list');
+Route::post('directory/map', [DirectoryController::class, 'map'])->name('directory.map');
 
-Route::get('about', 'HomeController@about')->name('about');
-Route::get('contact', 'HomeController@contact')->name('contact');
+Route::get('about', [HomeController::class, 'about'])->name('about');
+Route::get('contact', [HomeController::class, 'contact'])->name('contact');
 
-Route::get('legal-notice', 'PageController@legalNotice')->name('legal_notice');
-Route::get('cookies-policy', 'PageController@cookiesPolicy')->name('cookies_policy');
-Route::get('privacy-policy', 'PageController@privacyPolicy')->name('privacy_policy');
-Route::get('terms', 'PageController@terms')->name('terms');
+Route::get('legal-notice', [PageController::class, 'legalNotice'])->name('legal_notice');
+Route::get('cookies-policy', [PageController::class, 'cookiesPolicy'])->name('cookies_policy');
+Route::get('privacy-policy', [PageController::class, 'privacyPolicy'])->name('privacy_policy');
+Route::get('terms', [PageController::class, 'terms'])->name('terms');
 
 // Event
-Route::resource('event', 'EventController')->only(['index', 'show']);
-Route::post('event/datatable', 'EventController@datatable')->name('event.datatable');
+Route::resource('event', EventController::class)->only(['index', 'show']);
+Route::post('event/datatable', [EventController::class, 'datatable'])->name('event.datatable');
 
 // News
-Route::resource('news', 'NewsController')->only(['index', 'show']);
-Route::post('news/list', 'NewsController@list')->name('news.list');
+Route::resource('news', NewsController::class)->only(['index', 'show']);
+Route::post('news/list', [NewsController::class, 'list'])->name('news.list');
 
 // Demo case study
-Route::resource('demo', 'DemoCaseStudyController')->only(['index', 'show']);
-Route::post('demo/list', 'DemoCaseStudyController@list')->name('demo.list');
+Route::resource('demo', DemoCaseStudyController::class)->only(['index', 'show']);
+Route::post('demo/list', [DemoCaseStudyController::class, 'list'])->name('demo.list');
 
 // Media library
-Route::resource('media', 'MediaLibraryController')->only(['index', 'show'])->parameters([
+Route::resource('media', MediaLibraryController::class)->only(['index', 'show'])->parameters([
     'media' => 'media',
 ]);
-Route::post('media/list', 'MediaLibraryController@list')->name('media.list');
-Route::get('media/download/{media}', 'MediaLibraryController@download')->name('media.download');
+Route::post('media/list', [MediaLibraryController::class, 'list'])->name('media.list');
+Route::get('media/download/{media}', [MediaLibraryController::class, 'download'])->name('media.download');
 
 // Language
-Route::get('lang/{lang}', 'LanguageController@switchLang')->name('lang.switch');
+Route::get('lang/{lang}', [LanguageController::class, 'switchLang'])->name('lang.switch');
 
-Route::middleware('auth')->prefix('backend')->namespace('Backend')->name('backend.')->group(function () {
+Route::middleware('auth')->prefix('backend')->name('backend.')->group(function () {
 
     // Dashboard
-    Route::get('/', 'DashboardController@index')->name('dashboard');
+    Route::get('/', [Backend\DashboardController::class, 'index'])->name('dashboard');
 
     // Account
-    Route::get('account', 'AccountController@show')->name('account.show');
-    Route::get('account/edit-directory', 'AccountController@editDirectory')->name('account.edit_directory');
-    Route::match(['put', 'patch'], 'account', 'AccountController@updateDirectory')->name('account.update_directory');
-    Route::match(['put', 'patch'], 'account/update-communication', 'AccountController@updateCommunication')
+    Route::get('account', [Backend\AccountController::class, 'show'])->name('account.show');
+    Route::get('account/edit-directory', [Backend\AccountController::class, 'editDirectory'])->name('account.edit_directory');
+    Route::match(['put', 'patch'], 'account', [Backend\AccountController::class, 'updateDirectory'])->name('account.update_directory');
+    Route::match(['put', 'patch'], 'account/update-communication', [Backend\AccountController::class, 'updateCommunication'])
         ->name('account.update_communication');
-    Route::match(['put', 'patch'], 'account/update-user', 'AccountController@updateUser')->name('account.update_user');
-    Route::delete('account', 'AccountController@requestDestroy')->name('account.request_destroy');
-    Route::get('account/destroy/{user}', 'AccountController@destroy')->name('account.destroy')->middleware('signed');
+    Route::match(['put', 'patch'], 'account/update-user', [Backend\AccountController::class, 'updateUser'])->name('account.update_user');
+    Route::delete('account', [Backend\AccountController::class, 'requestDestroy'])->name('account.request_destroy');
+    Route::get('account/destroy/{user}', [Backend\AccountController::class, 'destroy'])->name('account.destroy')->middleware('signed');
 
     // Directory
-    Route::get('directory/{directory}/delete', 'DirectoryController@destroy')->name('directory.delete');
+    Route::get('directory/{directory}/delete', [Backend\DirectoryController::class, 'destroy'])->name('directory.delete');
     Route::resource('directory', 'DirectoryController');
-    Route::get('directory/{directory}/approve', 'DirectoryController@approve')->name('directory.approve');
-    Route::get('directory/{directory}/refuse', 'DirectoryController@refuse')->name('directory.refuse');
-    Route::post('directory/datatable', 'DirectoryController@datatable')->name('directory.datatable');
-    Route::match(['GET', 'POST'], 'directory/search', 'DirectoryController@search')->name('directory.search');
+    Route::get('directory/{directory}/approve', [Backend\DirectoryController::class, 'approve'])->name('directory.approve');
+    Route::get('directory/{directory}/refuse', [Backend\DirectoryController::class, 'refuse'])->name('directory.refuse');
+    Route::post('directory/datatable', [Backend\DirectoryController::class, 'datatable'])->name('directory.datatable');
+    Route::match(['GET', 'POST'], 'directory/search', [Backend\DirectoryController::class, 'search'])->name('directory.search');
 
     // Directory change
     Route::resource('directory-change', 'DirectoryChangeController');
-    Route::get('directory-change/{directory_change}/approve', 'DirectoryChangeController@approve')->name('directory-change.approve');
-    Route::get('directory-change/{directory_change}/refuse', 'DirectoryChangeController@refuse')->name('directory-change.refuse');
+    Route::get('directory-change/{directory_change}/approve', [Backend\DirectoryChangeController::class, 'approve'])->name('directory-change.approve');
+    Route::get('directory-change/{directory_change}/refuse', [Backend\DirectoryChangeController::class, 'refuse'])->name('directory-change.refuse');
 
     // Event
     Route::resource('event', 'EventController');
-    Route::get('event/{event}/approve', 'EventController@approve')->name('event.approve');
-    Route::get('event/{event}/refuse', 'EventController@refuse')->name('event.refuse');
-    Route::post('event/datatable', 'EventController@datatable')->name('event.datatable');
+    Route::get('event/{event}/approve', [Backend\EventController::class, 'approve'])->name('event.approve');
+    Route::get('event/{event}/refuse', [Backend\EventController::class, 'refuse'])->name('event.refuse');
+    Route::post('event/datatable', [Backend\EventController::class, 'datatable'])->name('event.datatable');
 
     // News
     Route::resource('news', 'NewsController');
-    Route::get('news/{news}/approve', 'NewsController@approve')->name('news.approve');
-    Route::get('news/{news}/refuse', 'NewsController@refuse')->name('news.refuse');
-    Route::post('news/datatable', 'NewsController@datatable')->name('news.datatable');
+    Route::get('news/{news}/approve', [Backend\NewsController::class, 'approve'])->name('news.approve');
+    Route::get('news/{news}/refuse', [Backend\NewsController::class, 'refuse'])->name('news.refuse');
+    Route::post('news/datatable', [Backend\NewsController::class, 'datatable'])->name('news.datatable');
 
     // Demo case study
     Route::resource('demo', 'DemoCaseStudyController');
-    Route::get('demo/{demo}/approve', 'DemoCaseStudyController@approve')->name('demo.approve');
-    Route::get('demo/{demo}/refuse', 'DemoCaseStudyController@refuse')->name('demo.refuse');
-    Route::post('demo/datatable', 'DemoCaseStudyController@datatable')->name('demo.datatable');
+    Route::get('demo/{demo}/approve', [Backend\DemoCaseStudyController::class, 'approve'])->name('demo.approve');
+    Route::get('demo/{demo}/refuse', [Backend\DemoCaseStudyController::class, 'refuse'])->name('demo.refuse');
+    Route::post('demo/datatable', [Backend\DemoCaseStudyController::class, 'datatable'])->name('demo.datatable');
 
     // Media library
     Route::resource('media', 'MediaLibraryController')->parameters([
         'media' => 'media',
     ]);
-    Route::get('media/{media}/approve', 'MediaLibraryController@approve')->name('media.approve');
-    Route::get('media/{media}/refuse', 'MediaLibraryController@refuse')->name('media.refuse');
-    Route::post('media/datatable', 'MediaLibraryController@datatable')->name('media.datatable');
+    Route::get('media/{media}/approve', [Backend\MediaLibraryController::class, 'approve'])->name('media.approve');
+    Route::get('media/{media}/refuse', [Backend\MediaLibraryController::class, 'refuse'])->name('media.refuse');
+    Route::post('media/datatable', [Backend\MediaLibraryController::class, 'datatable'])->name('media.datatable');
 
     Route::middleware('isAdmin')->group(function () {
         // Country
         Route::resource('country', 'CountryController')->except('show');
-        Route::post('country/datatable', 'CountryController@datatable')->name('country.datatable');
-        Route::match(['GET', 'POST'], 'country/search', 'CountryController@search')->name('country.search');
-        Route::get('country/{country}/unassign-contact', 'CountryController@unassignContact')
+        Route::post('country/datatable', [Backend\CountryController::class, 'datatable'])->name('country.datatable');
+        Route::match(['GET', 'POST'], 'country/search', [Backend\CountryController::class, 'search'])->name('country.search');
+        Route::get('country/{country}/unassign-contact', [Backend\CountryController::class, 'unassignContact'])
             ->name('country.unassign_contact');
 
         // About
-        Route::get('about', 'AboutController@edit')->name('about.edit');
-        Route::put('about', 'AboutController@update')->name('about.update');
+        Route::get('about', [Backend\AboutController::class, 'edit'])->name('about.edit');
+        Route::put('about', [Backend\AboutController::class, 'update'])->name('about.update');
 
         // Contact
-        Route::get('contact', 'ContactController@edit')->name('contact.edit');
-        Route::put('contact', 'ContactController@update')->name('contact.update');
+        Route::get('contact', [Backend\ContactController::class, 'edit'])->name('contact.edit');
+        Route::put('contact', [Backend\ContactController::class, 'update'])->name('contact.update');
     });
 });
 
 // Tool
-Route::post('tool/image-upload', 'ToolController@imageUpload')->name('tool.image-upload');
-Route::post('tool/file-upload', 'ToolController@fileUpload')->name('tool.file_upload');
-Route::post('tool/map-search', 'ToolController@mapSearch')->name('tool.map_search');
+Route::post('tool/image-upload', [ToolController::class, 'imageUpload'])->name('tool.image-upload');
+Route::post('tool/file-upload', [ToolController::class, 'fileUpload'])->name('tool.file_upload');
+Route::post('tool/map-search', [ToolController::class, 'mapSearch'])->name('tool.map_search');
 
 // Route::get('generate-locations', function () {
 //     $directories = \App\Directory::withoutGlobalScopes()->get();
